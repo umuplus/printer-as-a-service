141eacc73ef66778561b2b8f5223fc84bc07dc2e
umu+ <aydinumut82@gmail.com>
2019-08-13 19:03:35 +0300

    jobs by user implemented minor improvements

--- routes/__ui__/home/controller.class.js
+++ routes/__ui__/home/controller.class.js
@@ -1,8 +1,45 @@
 'use strict';
 
+const apps = [ 'copy', 'print', 'scan', 'custom' ];
+const is = require('is_js');
+const mongoose = require('../../../models');
+
+const JobModel = mongoose.model('Job');
+const UserModel = mongoose.model('User');
+
 class Controller {
+    static async jobs(req, res) {
+        try {
+            const user = await UserModel.single({ username: req.body.username });
+            if (!user || user.deleted) throw new Error('user');
+
+            const jobs = await JobModel.paginate({ user: user._id }, -1);
+            res.json(is.array(jobs) ? jobs : []);
+        } catch (e) {
+            res.json({ e: e.message });
+        }
+    }
+
     static async index(req, res) {
-        res.render('__ui__/index', { layout: 'ui' });
+        let $app = res.locals.$qs.val('app', 'print');
+        if (!apps.includes($app)) $app = 'print';
+        await Controller[$app](req, res);
+    }
+
+    static async copy(req, res) {
+        res.render('__ui__/copy', { layout: 'ui' });
+    }
+
+    static async custom(req, res) {
+        res.render('__ui__/custom', { layout: 'ui' });
+    }
+
+    static async print(req, res) {
+        res.render('__ui__/print/index', { layout: 'ui' });
+    }
+
+    static async scan(req, res) {
+        res.render('__ui__/scan', { layout: 'ui' });
     }
 }
 
--- routes/__ui__/home/index.js
+++ routes/__ui__/home/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const Controller = require('./controller.class');
 
 router.get('/', Controller.index);
+router.post('/jobs', Controller.jobs);
 
 module.exports = router;
